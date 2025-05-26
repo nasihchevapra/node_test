535e27d4b453843654ee8b63e733b3ff70d6de66
nasihchevapra <mohammednasihchevapra47@gmail.com>
2025-05-26 19:15:57 +0400

    y

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 10000;
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://nasih_chevapra:nasih%40MongoDB00@cluster0.hj7x5to.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0e")
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB connection error:', err));
 
